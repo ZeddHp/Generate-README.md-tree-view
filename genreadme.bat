70e9baeff331891c3a27049e04b1e11d3cdeeaf1
Valters Jargans <valters.jargans@gmail.com>
2023-03-26 02:12:21 +0200

    bat

---
+++ genreadme.bat
@@ -0,0 +1,36 @@
+@echo off
+
+rem Delete any existing tree.txt file
+if exist README.md (
+  del README.md
+)
+
+rem Generate the directory listing in tree view
+tree /a /f > README.md
+
+rem Delete the first 6 rows of the file
+more +6 README.md > tree_temp.txt
+del README.md
+ren tree_temp.txt README.md
+
+rem Add the ``` symbol at the beginning of the file
+echo ``` > tree_temp.txt
+type README.md >> tree_temp.txt
+del README.md
+ren tree_temp.txt README.md
+
+rem Add the ``` symbol at the end of the file
+echo ``` >> README.md
+
+
+rem Print a message indicating that the operation is complete
+echo Directory listing has been updated in tree.txt.
+
+rem Add a 1-second delay
+timeout /t 1 > nul
+
+rem Pause before closing the Command Prompt window
+pause > nul
+
+rem Close the Command Prompt window
+exit
